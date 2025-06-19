f592eff2f41a43a9dce53569eaf1513f432d4591
Lissie Aranda <xlssthx@yahoo.com>
2025-06-19 00:40:18 -0600

    Peliculas
    Carrusel funcionando, pendiente las flechas

--- Peliculas.js
+++ Peliculas.js
@@ -114,16 +114,16 @@ class Carousel {
 
 // Configuración inicial
 const items = [
-  "avatar.jpg",
-  "titanic.jpg",
-  "starwars.jpg",
-  "avengers.jpg",
-  "jurassic.jpg",
-  "pelicula6.jpg",
-  "pelicula7.jpg",
-  "pelicula8.jpg",
-  "pelicula9.jpg",
-  "pelicula10.jpg"
+  "p1.jpg",
+  "p2.jpg",
+  "p3.jpg",
+  "p4.jpg",
+  "p5.jpg",
+  "p6.jpg",
+  "p7.jpg",
+  "p8.jpg",
+  "p9.jpg",
+  "p10.jpg"
 ];
 let carousel;
 
